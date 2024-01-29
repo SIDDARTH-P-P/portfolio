f45539771a18d50d2364cfd34112577f37903b8e
SIDDARTH-P-P <sidhusidharth2711@gmail.com>
2024-01-29 22:52:26 +0530

    register page completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,24 @@ import Skills from "./page/skills";
 import About from "./page/about";
 import Projects from "./page/projects";
 import Footer from "./components/footer";
-import { Route,BrowserRouter, Routes } from "react-router-dom";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Contact from "./page/contact";
 
-export default function App(){
-  return(
+export default function App() {
+  return (
     <>
-   <BrowserRouter>
-   <Nav/>
-   <Home/>
-   <About/>
-   <Skills/>
-   <Projects/>
-   <Contact/>
-   <Footer/>
-   
-   <Routes>
-   </Routes>
-   </BrowserRouter>
+      <BrowserRouter>
+        <Nav />
+        <Home />
+        <About />
+        <Skills />
+        <Projects />
+        <Contact />
+        <Footer />
+
+        <Routes>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
